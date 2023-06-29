4500f72e3ec0811001a4aaa18dfdf77a97c68058
AnCap-60 <assasin149maks@gmail.com>
2023-06-29 15:18:53 +0300

    Добавьте файлы проекта.

---
+++ PalletWarehouse/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PalletWarehouse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
